9d4ef77e28fba344c785d34c8c9d128c931eb2bc
Gusti Nugroho <50318345+gustiNugroho@users.noreply.github.com>
2021-04-07 15:42:55 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/WEBLAPOR/Sistem-rekapitulasi-showroom-BdlMobilindo';
+$config['base_url'] = 'http://localhost/Sistem-rekapitulasi-showroom-BdlMobilindo';
 
 /*
 |--------------------------------------------------------------------------
