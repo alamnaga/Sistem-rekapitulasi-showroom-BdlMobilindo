6253e049fca6bdf963123c24a1b610e7a4bb85df
christopmarpaung <63889830+christopmarpaung@users.noreply.github.com>
2021-04-13 11:50:34 +0700

    config,controllers,view

--- application/views/tambah-mobil.php
+++ application/views/tambah-mobil.php
@@ -1,162 +1,112 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>Tambah Informasi Mobil</title>
-
-  <!-- Bootstrap core CSS -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- Custom styles for this template -->
-  <link href="assets/css/simple-sidebar.css" rel="stylesheet">
-
+  <title>Dashboard</title>
+  <link rel="stylesheet" type="text/css" href="assets/css/tambah-mobil.css">
+  <!-- CSS only -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+  <!-- js -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 </head>
 
-<body>
-
-  <div class="d-flex" id="wrapper">
-
-    <!-- Sidebar -->
-    <div class="border-right" id="sidebar-wrapper" style="background-color: black;">
-      <div class="sidebar-heading"><img src="assets/img/logo.jpg "width="200"></div>
-      <div class="list-group list-group-flush">
-        <!-- button dashboard -->
-        <button id="btnGroupDrop1" type="button" class="btn btn-secondary" aria-expanded="false" onclick="location.href='<?= base_url('Dashboard'); ?>'">
-          Dashboard
-        </button>
-        <!-- button mobil -->
-        <button id="btnGroupDrop1" type="button" class="btn btn-secondary" aria-expanded="false">
-            Mobil
+<body class="lihat-mobil">
+  <div class="row">
+    <div class="col-md-2">
+      <ul class="list-group">
+        <img src="assets/img/logo.jpg">
+        <div class="btn-group" role="group">
+          <button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
           </button>
-          <!-- button pembelian -->
-          <button id="btnGroupDrop1" type="button" class="btn btn-secondary" aria-expanded="false">
-            Pembelian
-          </button> 
-        <!-- button penjualan -->
-          <button id="btnGroupDrop1" type="button" class="btn btn-secondary" aria-expanded="false">
-            Penjualan
-          </button>
-          <!-- button laporan -->
-          <button id="btnGroupDrop1" type="button" class="btn btn-secondary" aria-expanded="false">
-            Laporan
-          </button>
+          <ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+            <li><a class="dropdown-item" href="#">Dropdown link</a></li>
+            <li><a class="dropdown-item" href="#">Dropdown link</a></li>
+          </ul>
         </div>
+        <li class="list-group-item">A second item</li>
+        <li class="list-group-item">A third item</li>
+        <li class="list-group-item">A fourth item</li>
+        <li class="list-group-item">And a fifth one</li>
+      </ul>
     </div>
-    <!-- /#sidebar-wrapper -->
-
-    <!-- Page Content -->
-    <div id="page-content-wrapper">
-
-      <nav class="navbar navbar-expand-lg navbar-light border-bottom" style="background-color: black;">
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-            <li class="nav-item active">
-            <h4 class="header"><img src=assets/img/header.png alt="header" style="width: 20px; height: 20px;">Selamat Datang</h4>
-            </li>
-        </ul>
-        </div>
-      </nav>
-
-      <div class="container-fluid">
-        <h3 style="color: orange; text-align: center; padding-top: 10px">Sistem Rekapitulasi Showroom</h3>
-        
-        <div class="card-wrapper pb-3">
-        <h5 style="padding: 10px;border-bottom: 2px solid white;">Tambah Informasi Mobil</h5>
+    <div class="col-md-10">
+      <h4 class="header"><img src=assets/img/header.png alt="header" style="width: 20px; height: 20px;"> Selamat Datang</h4>
+      <br>
+      <center>
+        <h3 class="title">Sistem Rekapitulasi Showroom</h3>
+      </center>
+      <div class="card-wrapper pb-3">
+        <h5 style="padding: 10px;border-bottom: 2px solid white;"> Tambah Informasi Mobil</h5>
         <div class="row g-2 align-items-center m-1">
-          <div class="col-3 ">
+          <div class="col-2 ">
             <label for="text" class="col-form-label">No. Polisi</label>
           </div>
-          <div class="col-8">
+          <div class="col-9 pr-2">
             <input type="text" name="" class="form-control">
           </div>
         </div>
         <div class="row g-2 align-items-center m-1">
-          <div class="col-3">
+          <div class="col-2 ">
             <label for="text" class="col-form-label">Jenis/Merk</label>
           </div>
-          <div class="col-8">
+          <div class="col-9">
             <input type="text" name="" class="form-control">
           </div>
         </div>
         <div class="row g-2 align-items-center m-1">
-          <div class="col-3 ">
+          <div class="col-2 ">
             <label for="text" class="col-form-label">Warna</label>
           </div>
-          <div class="col-8">
+          <div class="col-9">
             <input type="text" name="" class="form-control">
           </div>
         </div>
         <div class="row g-2 align-items-center m-1">
-          <div class="col-3 ">
+          <div class="col-2 ">
             <label for="text" class="col-form-label">Bahan Bakar</label>
           </div>
-          <div class="col-8">
+          <div class="col-9">
             <input type="text" name="" class="form-control">
           </div>
         </div>
         <div class="row g-2 align-items-center m-1">
-          <div class="col-3 ">
+          <div class="col-2 ">
             <label for="text" class="col-form-label">Tahun</label>
           </div>
-          <div class="col-8">
+          <div class="col-9">
             <input type="text" name="" class="form-control">
           </div>
         </div>
         <div class="row g-2 align-items-center m-1">
-          <div class="col-3 ">
+          <div class="col-2 ">
             <label for="text" class="col-form-label">Tgl Beli</label>
           </div>
-          <div class="col-8">
+          <div class="col-9">
             <input type="date" name="" class="form-control">
           </div>
         </div>
         <div class="row g-2 align-items-center m-1">
-          <div class="col-3 ">
+          <div class="col-2 ">
             <label for="text" class="col-form-label">Harga Jual</label>
           </div>
-          <div class="col-8">
+          <div class="col-9">
             <input type="text" name="" class="form-control">
           </div>
         </div>
         <div class="row g-2 align-items-center m-1 pb-2">
-          <div class="col-3 ">
+          <div class="col-2 ">
             <label for="text" class="col-form-label">Harga Beli</label>
           </div>
-          <div class="col-8">
+          <div class="col-9">
             <input type="text" name="" class="form-control">
           </div>
         </div>
         <button type="button" class="btn btn-danger">Batal</button>
         <button type="button" class="btn btn-success">Simpan</button>
       </div>
+
     </div>
-
-      </div>
-        
-      </div>
-    </div>
-    <!-- /#page-content-wrapper -->
-
-  </div>
-  <!-- /#wrapper -->
-
-  <!-- Bootstrap core JavaScript -->
-  <script src="assets/vendor/jquery/jquery.min.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  <!-- Menu Toggle Script -->
-  <script>
-    $("#menu-toggle").click(function(e) {
-      e.preventDefault();
-      $("#wrapper").toggleClass("toggled");
-    });
-  </script>
 
 </body>
 
