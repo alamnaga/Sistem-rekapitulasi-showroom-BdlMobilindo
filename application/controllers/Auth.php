1be7a768b2b742993acbcfd607c4aa782a00c47c
Gusti Nugroho <50318345+gustiNugroho@users.noreply.github.com>
2021-04-15 10:01:12 +0700

    Update Auth.php

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -74,9 +74,9 @@ class Auth extends CI_Controller
         $this->form_validation->set_rules('user', 'User', 'trim|required');
         $this->form_validation->set_rules('password', 'Password', 'trim|required');
         if ($this->form_validation->run() == false) {
-            // $this->load->view('auth/login');
+            $this->load->view('auth/login');
             // $this->load->view('auth/dashboard');
-            $this->load->view('lihat-laporan');
+            // $this->load->view('lihat-laporan');
         } else {
             //validasi succes
             //Method private
