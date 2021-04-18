462ac898d0dfc60e55028ce2d3ae7dd9fd44c1c5
Gusti Nugroho <50318345+gustiNugroho@users.noreply.github.com>
2021-04-18 21:07:47 +0700

    Penambahan icon, tambah penjualan dan pembelian

--- application/views/lihat-penjualan.php
+++ application/views/lihat-penjualan.php
@@ -12,7 +12,7 @@
     <!-- Bootstrap core CSS -->
     <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom styles for this template -->
-    <link href="assets/css/lihat-penjualan.css" rel="stylesheet">
+    <link href="assets/css/lihat-mobil.css" rel="stylesheet">
 
 </head>
 
@@ -23,25 +23,25 @@
         <div class="border-right" id="sidebar-wrapper" style="background-color: black;">
             <div class="sidebar-heading"><img src="assets/img/logo.jpg " width="200"></div>
             <div class="list-group list-group-flush">
-                <!-- button dashboard -->
+               <!-- button dashboard -->
                 <button id="btnGroupDrop1" type="button" class="btn btn-secondary" aria-expanded="false" onclick="location.href='<?= base_url('Dashboard'); ?>'">
-                    Dashboard
+                <img src="assets/img/home.png" style="width: 25px; "> Dashboard
                 </button>
                 <!-- button mobil -->
                 <button id="btnGroupDrop1" type="button" class="btn btn-secondary" aria-expanded="false">
-                    Mobil
+                <img src="assets/img/mobil.png" style="width: 25px; "> Mobil
                 </button>
                 <!-- button pembelian -->
                 <button id="btnGroupDrop1" type="button" class="btn btn-secondary" aria-expanded="false">
-                    Pembelian
+                <img src="assets/img/cart.png" style="width: 25px; "> Pembelian
                 </button>
                 <!-- button penjualan -->
                 <button id="btnGroupDrop1" type="button" class="btn btn-secondary" aria-expanded="false">
-                    Penjualan
+                <img src="assets/img/sales.png" style="width: 25px; ">  Penjualan
                 </button>
                 <!-- button laporan -->
                 <button id="btnGroupDrop1" type="button" class="btn btn-secondary" aria-expanded="false">
-                    Laporan
+                <img src="assets/img/laporan.png" style="width: 25px; ">  Laporan
                 </button>
             </div>
         </div>
